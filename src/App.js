7a514c2124facd8efd5367e04d1381696372d91e
Yashi Gupta <yashi.12019103@lpu.in>
2023-02-02 16:24:05 +0530

    Random-color

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
 import './App.css';
+import "./Quote.css";
+import axios from "axios"
 
-function App() {
+const App = () => {
+  const [quote, setQuote] = useState("");
+  const [author, setauthor] = useState("");
+  const [changeColor,setChangeColor] = useState('#451252');
+  const quoteAPI = async() => {
+    let arrayOfQuotes = [];
+    try{
+      const data = await axios.get("https://api.quotable.io/random");
+      arrayOfQuotes=data.data;
+      console.log(arrayOfQuotes);
+
+    }catch(error){
+      console.log(error);
+    }
+    try{
+      setQuote(arrayOfQuotes.content);
+      setauthor(arrayOfQuotes.author);
+    }catch(error){
+      console.log(error);
+
+    }
+
+    };
+    useEffect(() => {
+      quoteAPI();
+    }, []);
+    const handleClick=()=>
+    {
+      const randomColor= '#'+Math.random().toString(16).slice(2,8)
+      setChangeColor(randomColor)
+    }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{backgroundColor: `${changeColor}`}}>
+      <div className="quoteBox">
+        <div className="container">
+          <div className="quote">{quote}</div>
+          <div className="quote">{author}</div>
+          <div className="quoteButton"><button onClick={quoteAPI,handleClick}>Change Quote</button></div>
+        </div>
+      </div> 
+      
     </div>
   );
-}
+
+};
 
 export default App;
